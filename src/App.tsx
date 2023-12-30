2a415afe4145a9f4339a81cc3f54b4fe8041317b
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-12-30 14:23:45 +0100

    fix label not refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,12 @@ const optionsLangs = [
 function App() {
   function RefreshDelta() {
     // in range [0.2, 0.3]
-    let delta = Math.random()*0.1+0.2
+    let delta = Math.random() * 0.1 + 0.2
 
     // in range [4, 6]
     delta *= 20
 
-    delta *= ((MESSAGE.accuracy-50)/50)
+    delta *= ((MESSAGE.accuracy - 50) / 50)
     SetMessageDelta(delta.toFixed(2))
   }
 
@@ -60,11 +60,12 @@ function App() {
         defaultValue={options[0]}
         menuPosition="fixed"
         onChange={(change: any) => {
-          SetMessage({
-            ...MESSAGE,
-            // @ts-ignore
-            id: change.label
-          })
+          if (id == "metric")
+            SetMessage({ ...MESSAGE, "metric": change.label })
+          else if (id == "dataset")
+            SetMessage({ ...MESSAGE, "dataset": change.label })
+          else if (id == "langs")
+            SetMessage({ ...MESSAGE, "langs": change.label })
         }}
         theme={(theme) => ({
           ...theme,
@@ -73,10 +74,10 @@ function App() {
             primary25: '#ccc',
             primary: 'black',
           },
-          
+
         })}
         // parses labels as html
-        formatOptionLabel={function(data) {
+        formatOptionLabel={function (data) {
           return (
             <span dangerouslySetInnerHTML={{ __html: data.label }} />
           );
@@ -85,7 +86,7 @@ function App() {
     )
   }
 
-  
+
   // Make sure that RefreshDelta is called only once on load
   // and then whenever message changes
   useEffect(
@@ -101,10 +102,10 @@ function App() {
       <div className="Question">
         How much improvement on
         {SelectElement(optionsMetrics, "metric")}
-        is needed on the 
+        is needed on the
         {SelectElement(optionsDatasets, "dataset")}
         dataset on
-        {SelectElement(optionsLangs, "langs")} 
+        {SelectElement(optionsLangs, "langs")}
         to get
         <input
           className="AccuracyInput"
@@ -112,16 +113,19 @@ function App() {
           min="50" max="100" step="1"
           defaultValue="90"
           onInput={(change: any) => {
-              SetMessage({
-                ...MESSAGE,
-                "accuracy": Number(change.target.value),
-              })
-            }
+            SetMessage({
+              ...MESSAGE,
+              "accuracy": Number(change.target.value),
+            })
+          }
           }
         ></input>%
         system-level accuracy with humans?
       </div>
-      <div className="Answer" dangerouslySetInnerHTML={{__html: "Required Δ ≥ " + MESSAGE_DELTA + " " + MESSAGE.metric + " points"}}></div>
+      <div
+        className="Answer"
+        dangerouslySetInnerHTML={{ __html: "Required Δ ≥ " + MESSAGE_DELTA + " " + MESSAGE.metric + " points"}}
+      ></div>
     </div>
   );
 }
