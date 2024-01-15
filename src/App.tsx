58128185518a24080b46fd5246cf0f58cc0a85f7
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-01-15 11:20:31 +0100

    numer display

--- src/App.tsx
+++ src/App.tsx
@@ -30,23 +30,23 @@ const BIBTEX = `
 
 const ACCURACY_POPUP = (
   <Popup trigger={<img src="question_circle.svg" className="question_circle"></img>} position="right center" offsetX={140}>
-      <div id="accuracy_popup">
+    <div id="accuracy_popup">
       The accuracy is empirically determined on particular dataset and does not reflect all scenarios. It mainly allows to compare various metrics and their estimated accuracy.<br></br>
       As an example, accuracy of 80% means, that for 80% of system pairs, the metric would make the same prediction (one system being better than another one) as a human would.
-      </div>
+    </div>
   </Popup>
 )
 
-function ComputeAccuracy(delta: number, metric: string) : number {
+function ComputeAccuracy(delta: number, metric: string): number {
   // @ts-ignore
   let [a, b] = thresholds[metric];
-  return a * (1/(1 + Math.pow(Math.E, -b * delta)))
+  return a * (1 / (1 + Math.pow(Math.E, -b * delta)))
 }
 
-function ComputeDelta(accuracy: number, metric: string) : number {
+function ComputeDelta(accuracy: number, metric: string): number {
   // @ts-ignore
   let [a, b] = thresholds[metric];
-  return -Math.log(a / accuracy - 1)/b
+  return -Math.log(a / accuracy - 1) / b
 }
 
 function App() {
@@ -55,7 +55,7 @@ function App() {
     metric: optionsMetrics[0],
   });
 
-  
+
   function SelectElement(options: { "label": string, "value": string }[], id: string) {
     return (
       <Select
@@ -94,35 +94,35 @@ function App() {
 
   let other_metrics = (
     optionsMetrics
-    .filter((x) => x.label !== MESSAGE_IN.metric.label)
-    .map((x) => [x.label, ComputeDelta(current_accuracy, x.value).toFixed(3)])
-    .map((x) => <li>
+      .filter((x) => x.label !== MESSAGE_IN.metric.label)
+      .map((x) => [x.label, Math.abs(ComputeDelta(current_accuracy, x.value)).toFixed(x.value === "comet21qe" ? 2 : 1).replace("NaN", "✕")])
+      .map((x) => <li>
         <span className="number_sector">{x[1]}</span>&nbsp;
         <span className='label_sector'>difference in</span>&nbsp;
-        <span className="metric_sector" dangerouslySetInnerHTML={{ __html: x[0] as string}}></span>
+        <span className="metric_sector" dangerouslySetInnerHTML={{ __html: x[0] as string }}></span>
       </li>)
   )
 
   let MESSAGE_OUT = (
-  <div id="message_out">
-    <div
-      style={{textAlign: 'center'}}
-      dangerouslySetInnerHTML={{ __html: `The system-level ${MESSAGE_IN.delta} ${MESSAGE_IN.metric.label} difference has:` }}>  
-    </div>
+    <div id="message_out">
+      <div
+        style={{ textAlign: 'center' }}
+        dangerouslySetInnerHTML={{ __html: `The system-level ${MESSAGE_IN.delta} ${MESSAGE_IN.metric.label} difference has:` }}>
+      </div>
 
-    <div
-      style={{textAlign: 'center', marginBottom: "20px"}}>
-      <span className="accuracy_sector">{current_accuracy.toFixed(1)}%</span> accuracy with humans {ACCURACY_POPUP}
-    </div>
-    
-    <div
-      style={{textAlign: 'center', marginBottom: "-20px"}}>
-      Which is the same as the following metrics:
-    </div>
-    
-    <ul>
-      {other_metrics}
-    </ul>
+      <div
+        style={{ textAlign: 'center', marginBottom: "20px" }}>
+        <span className="accuracy_sector">{current_accuracy.toFixed(1)}%</span> accuracy with humans {ACCURACY_POPUP}
+      </div>
+
+      <div
+        style={{ textAlign: 'center', marginBottom: "-20px" }}>
+        Which is the same as the following metrics:
+      </div>
+
+      <ul>
+        {other_metrics}
+      </ul>
     </div>
   )
 
@@ -149,12 +149,12 @@ function App() {
       </div>
       {MESSAGE_OUT}
       <div id="DisclaimerBar">
-      Please read the <a href="https://arxiv.org/pdf/2401.06760.pdf">paper by Kocmi, Zouhar, Federmann, Post (2024)</a> to see how all of this works.
-      See&nbsp;
-      <Popup trigger={<span id="bibtex_button">BibTeX for citation</span>} position="top center" offsetY={10}>
-        <div id="bibtex_popup"><pre>{BIBTEX}</pre></div>
-      </Popup>.
-      See <a href="https://github.com/kocmitom/MT-Thresholds">code</a>.
+        Please read the <a href="https://arxiv.org/pdf/2401.06760.pdf">paper by Kocmi, Zouhar, Federmann, Post (2024)</a> to see how all of this works.
+        See&nbsp;
+        <Popup trigger={<span id="bibtex_button">BibTeX for citation</span>} position="top center" offsetY={10}>
+          <div id="bibtex_popup"><pre>{BIBTEX}</pre></div>
+        </Popup>.
+        See <a href="https://github.com/kocmitom/MT-Thresholds">code</a>.
       </div>
     </div>
 
